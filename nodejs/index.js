f15ad36f0cb68844ec1f683360014613914a4c06
kartikeypatel1 <kp0543985@gmail.com>
2026-02-04 10:07:42 +0530

    created five route

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,22 @@
 const http=require('http');
 const myserver=http.createServer((req,res)=>{
-    console.log('server1');
-    res.end('hello nodejs');
+    if(req.url=='/'){
+        res.end(
+            `<h1>Welcome to Home Page</h1>
+            <img src="https://images.unsplash.com/photo-1769109003123-99c12ae4b23c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D" alt="ABESIT Logo" width:200px >  `
+        );
+    }
+    else if(req.url=='/about'){
+        res.end('we are student of ABES');
+    }else if(req.url=='/contact'){
+        res.end('1234567890');
+    }
+    else if(req.url=='/class'){
+        res.end('IT-A');
+    }
+    else{
+        res.end('404 page not found');
+    }
 });
 myserver.listen(8000,(req,res)=>{
     console.log('server is running on port 8000');
